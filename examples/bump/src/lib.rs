41c475466e70631b81f8196ddd31b9dcbf58a6aa
Michael Hewson <michael@michaelhewson.ca>
2022-03-23 00:38:26 -0700

    add test using bumpalo_herd

---
+++ examples/bump/src/lib.rs
@@ -0,0 +1,46 @@
+#![cfg(test)]
+use borrowed_with_owner::{BorrowFromOwner, RefWithOwner};
+use bumpalo_herd::Herd;
+use std::sync::Arc;
+
+struct BorrowSliceOfStrs;
+
+impl<'a> BorrowFromOwner<'a> for BorrowSliceOfStrs {
+    type Borrowed = &'a [&'a str];
+}
+
+#[test]
+fn test_bumpalo_herd() {
+    let herd = RefWithOwner::new(Arc::new(Herd::new()));
+
+    let titles_with_herd = herd.map::<BorrowSliceOfStrs, _>(|herd, _| {
+        let member = herd.get();
+
+        let iter = (0..5).map(|i| {
+            // note: bumpalo_herd doesn't support bumpalo's `format!` macro yet,
+            // so I'm just using the regular `format!` macro for this demo.
+            let string = format!("Mambo number {}", i);
+
+            member.alloc_str(&string) as &_
+        });
+
+        member.alloc_slice_fill_iter(iter)
+    });
+
+    for _ in 0..2 {
+        let titles_with_herd = titles_with_herd.clone();
+        std::thread::spawn(move || {
+            let slice = titles_with_herd.borrowed();
+            assert_eq!(
+                slice,
+                &[
+                    "Mambo number 0",
+                    "Mambo number 1",
+                    "Mambo number 2",
+                    "Mambo number 3",
+                    "Mambo number 4",
+                ]
+            );
+        });
+    }
+}
